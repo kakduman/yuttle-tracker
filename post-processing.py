19ddea682a60ed5aec4c4ffb0f148150a2272c93
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-05-04 01:24:33 -0400

    Refactor arrival times

--- post-processing.py
+++ post-processing.py
@@ -3,4 +3,5 @@ import os
 
 # go through every file in the data directory and run append_arrival_times on it, saving it as .json instead of .txt
 for file in os.listdir('data/raw_data'):
-    append_arrival_times(f'data/raw_data/{file}', f'data/processed_data/arrival_data/{file[:-4]}.json')
+    if file.endswith('.txt'):
+        append_arrival_times(f'data/raw_data/{file}', f'data/processed_data/arrival_data/{file[:-4]}.json')
--- utils/get_arrival_times.py
+++ utils/get_arrival_times.py
@@ -1,9 +1,8 @@
-import json
 import os
 import gzip
 import pickle
 
-
+MINUTES_IN_BUS_DAY = 660
 route_stops = {1: [10, 2, 5, 52, 41, 20, 108, 106, 34, 101, 47, 100, 102, 105, 69, 139, 136, 130, 129, 140, 133, 135, 138, 97, 66, 63, 42, 98, 38, 39, 72, 43]}
 arrivals_dict = {}
 
@@ -22,85 +21,67 @@ def is_line_invalid(line):
     
     return False
 
+def reset_arrivals_dict(arrivals_dict, route):
+    for stop in route_stops[route]:
+        arrivals_dict[stop] = None
 
-def append_arrival_times(input_file, output_file):
+
+def append_arrival_times(input_file, output_file, route=1):
     '''
     Appends arrival times to a given input file, returning a new file with the arrival times.
     
     Parameters:
     input_file (str): The file to read from.
     output_file (str): The file to write to.
+    route (int): The route number of analysis. Defaults to 1 (blue).
     '''
     
     # Function goes in reverse order, finding when the lastStop changed and adding this time to an arrivals dict
     # Then, each line is modified with its corresponding arrival times, and re-reversed
     
-    for stop in route_stops[1]:
-        arrivals_dict[stop] = None
+    reset_arrivals_dict(arrivals_dict, route)
 
     with open(input_file, "r") as file:
-        previously_recorded_stop = None
-        # unix_time is infinite
+        prev_stop = None
         unix_time = 99999999999
         updated_lines = []
 
-        # reverse the lines
-        try:
-            lines = file.readlines()
-        except Exception as e:
-            print(f"Error reading {input_file}: {e}")
-            return
+        lines = file.readlines()
         lines.reverse()
         
-
         for line in lines:
             # convert raw text to dictionary
             line = eval(line)
-            unix_time = line["lastUpdate"]
 
             if is_line_invalid(line):
                 continue
 
             # when the date changes (time decreases by MORE than 10800 in one go), reset the arrivals_dict
+            unix_time = line["lastUpdate"]
             if line["lastUpdate"] < (unix_time - 10800):
-                # reset arrivals_dict
-                for stop in route_stops[1]:
-                    arrivals_dict[stop] = None
+                reset_arrivals_dict(arrivals_dict, route)
             
-            if not previously_recorded_stop:
-                previously_recorded_stop = line["lastStop"]
-                for estimate in line["estimatedTimes"]:
-                    estimate_in_dayTime = line["estimatedTimes"][estimate] / 660
-                    try:
-                        arrival_estimate = round(estimate_in_dayTime + line["dayPercent"], 5)
-                        line["estimatedTimes"][estimate] = arrival_estimate
-                    except Exception as e:
-                        print(f"Error appending updated lines: {e}")
-                        continue
-                line["arrivals"] = dict(arrivals_dict)
-                updated_lines.append(line)
+            if not prev_stop: 
+                prev_stop = line["lastStop"]
                 continue
             
+            # update arrivals_dict if the stop changed
             current_stop = line["lastStop"]
-            if current_stop != previously_recorded_stop:
-                # arrivals_dict[current_stop] is set to the previous line's dayPercent
+            if current_stop != prev_stop:
                 arrivals_dict[current_stop] = line["dayPercent"]
-                previously_recorded_stop = current_stop
-            # append arrivals_dict to the line
-            # copied because it avoids future changes from affecting it, since dict is mutable
-            line["arrivals"] = dict(arrivals_dict)
+                prev_stop = current_stop
+            
+            line["arrivals"] = arrivals_dict.copy()
+
+            # transform estimatedTimes to day percentages
             for estimate in line["estimatedTimes"]:
-                estimate_in_dayTime = line["estimatedTimes"][estimate] / 660
-                try:
-                    arrival_estimate = round(estimate_in_dayTime + line["dayPercent"], 5)
-                    line["estimatedTimes"][estimate] = arrival_estimate
-                    updated_lines.append(line)
-                except Exception as e:
-                    print(f"Error appending updated lines: {e}")
-                    continue
+                estimate_in_dayTime = line["estimatedTimes"][estimate] / MINUTES_IN_BUS_DAY
+                arrival_estimate = round(estimate_in_dayTime + line["dayPercent"], 5)
+                line["estimatedTimes"][estimate] = arrival_estimate
+            
+            updated_lines.append(line)
+
         updated_lines.reverse()
-        
-        # save to a new file .json, make directory if it doesn't exist
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
         with gzip.open(output_file + '.gz', 'wb') as new_file:
             pickle.dump(updated_lines, new_file)
