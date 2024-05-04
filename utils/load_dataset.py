559394207bb2cfcfad42aec7bdcb94a32c9f1837
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-05-03 21:20:49 -0400

    Add function to load only estimate and arrival times from the dataset

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -1,5 +1,3 @@
-__all__ = ["load_dataset"]
-
 import gzip
 import pickle
 import os
@@ -21,7 +19,7 @@ def add_bus_data(dataset, file_path):
             raw_processed_datapoint["lastStop"],
             raw_processed_datapoint["dayPercent"],
             raw_processed_datapoint["weekday"],
-            raw_processed_datapoint["estimatedTimes"] # this is a dictionary containing 32 ground truths!
+            raw_processed_datapoint["arrivals"] # this is a dictionary containing 32 ground truths!
         ]
         dataset.append(clean_datapoint)
 
@@ -44,7 +42,37 @@ def load_dataset(route=1):
                 add_bus_data(dataset, file_path)
     return dataset
 
+def add_bus_prediction_data(dataset, file_path):
+    raw_processed_data = read_compressed_data(file_path)
+    for raw_processed_datapoint in raw_processed_data:
+        if "arrivals" not in raw_processed_datapoint:
+            continue
+        
+        clean_datapoint = [
+            raw_processed_datapoint["estimatedTimes"],
+            raw_processed_datapoint["arrivals"]
+        ]
+        dataset.append(clean_datapoint)
+
+def load_estimate_dataset(route=1):
+    """
+    Loads the official estimated arrival times and the actual arrival times for a given route into an array
+
+    Args:
+        route (int): The route number to load data for. Defaults to 1 (blue)
+
+    Returns:
+        N x d array; d = 2 with the first column as dictionaries of the estimated arrival times and the second column as dictionaries of the actual arrival times
+    """
+    dataset = []
+    for filename in os.listdir(DATA_DIRECTORY):
+        if filename.startswith(f"bus_{route}_") and filename.endswith(".json.gz"):
+            file_path = os.path.join(DATA_DIRECTORY, filename)
+            if os.path.isfile(file_path):
+                add_bus_prediction_data(dataset, file_path)
+    return dataset
+
 
 if __name__ == '__main__':
-    dataset = load_dataset()
-    print(dataset[0])
+    dataset = load_estimate_dataset()
+    print(dataset[100])
